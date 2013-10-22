97db640ee3ce1c2fe12cb18c5ddc040c0ac381b0
Carlos Guimarães <cguimaraes@av.it.pt>
2013-10-22 15:14:47 +0100

    Changed license to LGPL v2.1

--- src/mih/types/qos/auxiliar/ListMaxPkTxDelay.java
+++ src/mih/types/qos/auxiliar/ListMaxPkTxDelay.java
@@ -4,15 +4,24 @@
 //------------------------------------------------------------------------------
 // ODTONE - Open Dot Twenty One
 //
-// Copyright (C) 2009-2013 Universidade Aveiro
-// Copyright (C) 2009-2013 Instituto de Telecomunicações - Pólo Aveiro
+// Copyright (C) 2013 Universidade Aveiro
+// Copyright (C) 2013 Instituto de Telecomunicações - Pólo Aveiro
 //
-// This software is distributed under a license. The full license
-// agreement can be found in the file LICENSE in this distribution.
-// This software may not be copied, modified, sold or distributed
-// other than expressed in the named license agreement.
+// This file is part of MIH-JAVA-Library.
 //
-// This software is distributed without any warranty.
+// MIH-JAVA-Library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// MIH-JAVA-Library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with MIH-JAVA-Library. If not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 //==============================================================================
 
 package mih.types.qos.auxiliar;
